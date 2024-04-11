db8f3cc857d997417aefa317bebe51f223a31523
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-11 15:42:48 +1000

    Update main.py

--- String formatting: format and f-strings/Task 1. Order/main.py
+++ String formatting: format and f-strings/Task 1. Order/main.py
@@ -1,1 +1,7 @@
+name_user = input(f"Введите имя: ")
+number_order = int(input("Введите номер заказа: "))
+
+message = "\nЗдравствуйте, {0}! Ваш номер заказа: {1}. Приятного дня!".format(name_user, number_order)
+
+print(message)
 
