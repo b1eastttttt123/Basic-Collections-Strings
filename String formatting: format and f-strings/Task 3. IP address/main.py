2165bf5de05a954024ecda5bc0794a5157415e81
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-11 15:56:15 +1000

    Update main.py

--- String formatting: format and f-strings/Task 3. IP address/main.py
+++ String formatting: format and f-strings/Task 3. IP address/main.py
@@ -1,1 +1,7 @@
+import random
 
+ip_address = [random.randint(0, 255) for _ in range(4)]
+
+message = "{0}.{1}.{2}.{3}".format(ip_address[0], ip_address[1], ip_address[2], ip_address[3])
+
+print(f"IP-адрес: {message}")
