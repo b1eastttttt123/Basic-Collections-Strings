7a1a8f2673d2b0c28583fd139ddb6b6674b5a53c
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-11 15:49:49 +1000

    Update main.py

--- String formatting: format and f-strings/Task 2. Debts/main.py
+++ String formatting: format and f-strings/Task 2. Debts/main.py
@@ -1,1 +1,6 @@
+name_debtor = input("Введите имя должника: ")
+dept = int(input(f"Введите долг, {name_debtor}: "))
 
+message = "{0}! {0}, привет! Как дела, {0}? Где мои {1} рублей? {0}!".format(name_debtor, dept)
+
+print(message)
