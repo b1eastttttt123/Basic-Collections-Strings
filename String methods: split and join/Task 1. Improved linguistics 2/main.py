07fd518fa118f9c677329b27b93c9273186b27dc
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-15 05:03:39 +1000

    Update main.py

--- String methods: split and join/Task 1. Improved linguistics 2/main.py
+++ String methods: split and join/Task 1. Improved linguistics 2/main.py
@@ -1,1 +1,7 @@
+def findWord():
+    user_text_list, text_list = [input(f"Введите {num + 1} слово: ") for num in range(3)], input("Введите любой текст: ")
 
+    for word in user_text_list:
+        print(f"Слово {word} встречается в тексте {text_list.count(word)} раз")
+
+findWord()
