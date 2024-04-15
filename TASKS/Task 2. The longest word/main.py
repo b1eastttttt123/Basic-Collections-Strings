d939d800aa94adfb83309a99cfddd14bddcf2263
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-15 19:25:33 +1000

    Update main.py

--- TASKS/Task 2. The longest word/main.py
+++ TASKS/Task 2. The longest word/main.py
@@ -1,1 +1,8 @@
+import math
+
+text_user = input('Введите строку: ')
+
+max_word = max(text_user.split(), key=len)
+
+print(f"Самое длинное слово: «{max_word}».", f"Длина этого слова: {len(max_word)} символов.", sep="\n")
 
