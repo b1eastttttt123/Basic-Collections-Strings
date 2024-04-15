183c8426b8a7fac43f8db7ff0af916f161a04ff7
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-15 15:39:06 +1000

    Update main.py

--- String methods: startswith, endswith, upper, lower/Task 1. Caesar's Cipher 2/main.py
+++ String methods: startswith, endswith, upper, lower/Task 1. Caesar's Cipher 2/main.py
@@ -1,1 +1,17 @@
+alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
+step = int(input("Шаг шифровки: "))
+message = input("Введите сообщение: ").upper()
+result = ""
+
+for letter in message:
+
+    place = alphabet.find(letter)
+    new_place = place + step
+
+    if letter in alphabet:
+        result += alphabet[new_place]
+    else:
+        result += letter
+
+print("Зашифрованное сообщение:", result)
